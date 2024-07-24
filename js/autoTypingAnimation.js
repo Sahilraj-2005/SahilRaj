d2bb58d3776ceb4eafa979ae86e4bcf943498687
Sahilraj-2005 <sahilrajsingh2005@gmail.com>
2024-07-24 13:37:28 +0530

    commit

--- js/autoTypingAnimation.js
+++ js/autoTypingAnimation.js
@@ -1,7 +1,7 @@
 setTimeout(function() {
     $('.about-info-inner').typed({
       strings: [
-        "<span>Hi, I am Golam Morsed</span> Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit eius officia ea ut vonsectetur adipisicing elit. Illo voluptatum iste, tempora rem obcaecati omnis delectus sunt, consequuntur, et ut quasi aperiam assumenda! <br><br> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex beatae reprehenderit vero maiores dignissimos error vitae, optio inventore explicabo quia officiis magni delectus? Lorem ipsum dolor, sit amet consectetur"
+        "<span>Hi, I am Sahil Raj</span> To secure a challenging position in a reputable organization to expand my learning, knowledge, and skills As a Mining Engineering student with experience in English, Python, and C++, I am seeking a position as a software engineer and data scientist. My passion for coding and hobbies in basketball and traveling have given me a unique perspective on problem-solving. I am eager to apply my skills and education to contribute to a dynamic team. <br>"
       ],
       typeSpeed: 7,
       contentType: 'html'
