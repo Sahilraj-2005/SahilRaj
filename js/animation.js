7c6b7bdf1e20e9d2a9aa168263a0ed3f908d5082
Sahilraj-2005 <sahilrajsingh2005@gmail.com>
2025-07-25 00:28:08 +0530

    Final year update

--- js/animation.js
+++ js/animation.js
@@ -1,7 +1,7 @@
 const typedTextSpan = document.querySelector(".typed-text");
 const cursorSpan = document.querySelector(".cursor");
 
-const textArray = ["Sahil Raj", "Student", "Web developer", "Problem solver"];
+const textArray = ["Sahil Raj", "Student", "AI Cloud developer", "Problem solver"];
 const typingDelay = 200;
 const erasingDelay = 100;
 const newTextDelay = 2000; // Delay between current and next text
--- js/autoTypingAnimation.js
+++ js/autoTypingAnimation.js
@@ -1,7 +1,7 @@
 setTimeout(function() {
     $('.about-info-inner').typed({
       strings: [
-        "<span>Hi, I am Sahil Raj</span> To secure a challenging position in a reputable organization to expand my learning, knowledge, and skills As a Mining Engineering student with experience in English, Python, and C++, I am seeking a position as a software engineer and data scientist. My passion for coding and hobbies in basketball and traveling have given me a unique perspective on problem-solving. I am eager to apply my skills and education to contribute to a dynamic team. <br>"
+        "<span>Hi, I am Sahil Raj</span> I'm a final-year student at NITK, pivoting into Data Engineering and AI Development, with hands-on experience building end-to-end MLOps pipelines using ZenML, MLflow, and CI/CD, as well as designing scalable Azure cloud architectures (ADF, Databricks, Synapse) that deliver 35% faster data ingestion and power REST-API-backed services. Skilled in PySpark, SQL, PowerBI, and Git, I've deployed a 97%-accurate production-ready house price prediction model and led cross-functional projects to drive business insights. I'm actively seeking opportunities in Data Engineering and AI Development—let's connect!<br>"
       ],
       typeSpeed: 7,
       contentType: 'html'
